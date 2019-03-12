7fc3f7eb4b3158fde194523e8880c2dbdf828547
Carula <carulagarat@gmail.com>
2019-03-13 00:39:27 +0100

    clean

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainLayout from './layouts/MainLayout';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<MainLayout />, document.getElementById('root'));
-
-serviceWorker.unregister();
