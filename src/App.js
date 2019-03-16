95d0f16c908865b2bbca7dbfcb76bc193cc9fdc1
Carula <carulagarat@gmail.com>
2019-03-16 19:01:33 +0100

    app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="http://www.myiconfinder.com/uploads/iconsets/256-256-0bcdda32f766556c76b6c08b485acef7.png" className="App-logo" alt="logo" />
+          <p>
+            On it...
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
