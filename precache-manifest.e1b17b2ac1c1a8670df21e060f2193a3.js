88eb10e280e19a6e86f225c56b3b6cd9a0c4e477
Carula <carulagarat@gmail.com>
2019-03-16 23:16:14 +0100

    Updates

---
+++ precache-manifest.e1b17b2ac1c1a8670df21e060f2193a3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d6566b1d696502619fc3275d44f1485",
+    "url": "/static/media/Rubik-BoldItalic.7d6566b1.woff2"
+  },
+  {
+    "revision": "b64fb1b6f7353db7b950",
+    "url": "/static/css/main.9378205e.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b60fd308b7d087f10ae",
+    "url": "/static/js/2.1216f8fa.chunk.js"
+  },
+  {
+    "revision": "bb3629c9e9df63514061aa82f99e5e76",
+    "url": "/static/media/RubikMonoOne-Regular.bb3629c9.woff2"
+  },
+  {
+    "revision": "a584ece1ef48e3f6a97e029a6cefedd7",
+    "url": "/static/media/RubikMonoOne-Regular.a584ece1.woff"
+  },
+  {
+    "revision": "cc747949586b308802e79fd161fbf3be",
+    "url": "/static/media/Rubik-Light.cc747949.woff2"
+  },
+  {
+    "revision": "4dd6fb5a834ae523ab510815dd963ebb",
+    "url": "/static/media/Rubik-Regular.4dd6fb5a.woff2"
+  },
+  {
+    "revision": "4cefa8870b0cdda166dcf4cb04608d53",
+    "url": "/static/media/Rubik-Bold.4cefa887.woff2"
+  },
+  {
+    "revision": "b64fb1b6f7353db7b950",
+    "url": "/static/js/main.c9ccc8db.chunk.js"
+  },
+  {
+    "revision": "1e5928354dba90e59762004a24074978",
+    "url": "/static/media/Rubik-Italic.1e592835.woff2"
+  },
+  {
+    "revision": "dee3036437b9f88f3ce43f4b814ff0c9",
+    "url": "/static/media/Rubik-LightItalic.dee30364.woff2"
+  },
+  {
+    "revision": "a28c30f336818ae6e12c045e9325b8a3",
+    "url": "/static/media/Rubik-Regular.a28c30f3.woff"
+  },
+  {
+    "revision": "9133fdc7b7c1a95275a98b0f3cc1c345",
+    "url": "/static/media/Rubik-Bold.9133fdc7.woff"
+  },
+  {
+    "revision": "aee2678c95c41b59a9ea2463bd069329",
+    "url": "/static/media/Rubik-LightItalic.aee2678c.woff"
+  },
+  {
+    "revision": "7b4520cfbc1a061169565108b555f9ab",
+    "url": "/static/media/Rubik-Light.7b4520cf.woff"
+  },
+  {
+    "revision": "b54df73354d6f6b4f26cf43e73899544",
+    "url": "/static/media/Rubik-BoldItalic.b54df733.woff"
+  },
+  {
+    "revision": "23ba1ccc25532cec67260dedd072b8c1",
+    "url": "/static/media/Rubik-Italic.23ba1ccc.woff"
+  },
+  {
+    "revision": "4a5e4d408d173f182a2de9bfc194a196",
+    "url": "/index.html"
+  }
+];
